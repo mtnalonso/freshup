4c154d308f87d634c14554de57624de06e08f59d
Martin Alonso <m13alonso@gmail.com>
2018-06-21 23:57:10 +0200

    Add tig installation

--- freshup.sh
+++ freshup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-
-sudo apt-get install -y \
-    vim \
-    curl \
-    wget \
-    git \
-    tmux \
-
-wget https://raw.githubusercontent.com/mtnalonso/vimrc/master/.vimrc
-
 function install_vim-plug {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
@@ -20,3 +10,27 @@ function c_installation {
         gdb \
         make
 }
+
+function install_tig {
+    tig_version='tig-2.3.3'
+    sudo apt-get install -y libncurses5-dev libncursesw5-dev
+    wget https://github.com/jonas/tig/releases/download/$tig_version/$tig_version.tar.gz
+    tar -xzf $tig_version.tar.gz
+    make -C $tig_version prefix=/usr/local
+    sudo make -C $tig_version install prefix=/usr/local
+    rm -rf $tig_version*
+}
+
+
+sudo apt-get install -y \
+    vim \
+    git \
+    curl \
+    wget \
+    tmux \
+
+wget https://raw.githubusercontent.com/mtnalonso/vimrc/master/.vimrc
+
+install_vim-plug
+c_installation
+install_tig
